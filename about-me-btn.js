143b9264a724a11af9cf77df5c913e24c58476bc
Adritomustafa153 <amustafa191065@bscse.uiu.ac.bd>
2024-08-04 17:24:34 +0600

    About me buton corrected

--- about-me-btn.js
+++ about-me-btn.js
@@ -1,3 +1,3 @@
 document.getElementById('redirect-button').addEventListener('click', function() {
-    window.location.href = 'About/About.html'; // Replace with the URL you want to redirect to
+    window.location.href = 'About.html'; // Replace with the URL you want to redirect to
 });
