bed759f78e55f4a310a437d577364678b6a4e6b4
Adritomustafa153 <amustafa191065@bscse.uiu.ac.bd>
2024-08-13 16:12:54 +0600

    Slider partially added on Hobby page

--- slider.js
+++ slider.js
@@ -1,14 +1,35 @@
-let currentIndex = 0;
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+    
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+     
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 
-const slides = document.querySelector('.slides');
-const totalSlides = slides.children.length;
+    // Responsive Break Points
+    breakpoints:{
+        0:{
+            slidesPreView: 1
+        },
+        620:{
+            slidesPreView: 2
+        },
+        1024:{
+            slidesPreView: 3
+        }
+    }
 
-document.getElementById('next').addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % totalSlides;
-    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
-});
 
-document.getElementById('prev').addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
-});
+  });
